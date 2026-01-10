241134b1558e5f373daf415d1dc6822cb9f8b31b
jiang qiang <jiangtingqiang@gmail.com>
2026-01-10 21:10:14 +0800

    refactor: tweak the path

--- src/service/global/proj/proj_util.rs
+++ src/service/global/proj/proj_util.rs
@@ -3,22 +3,9 @@ use rust_wheel::{
     config::app::app_conf_reader::get_app_config,
     texhub::project::get_proj_path,
 };
-use serde_json::Value;
-
-pub fn get_proj_compile_req(proj_id: &String, file_name: &String) -> Value {
-    let file_path = format!("/opt/data/project/{}/{}", proj_id, file_name);
-    let out_path = format!("/opt/data/project/{}", &proj_id);
-    let json_data = serde_json::json!({
-        "file_path": file_path,
-        "out_path": out_path,
-        "req_time": get_current_millisecond(),
-        "project_id": proj_id
-    });
-    return json_data;
-}
 
 pub fn get_proj_base_dir(proj_id: &String, created_time: i64) -> String {
-    let base_compile_dir: String = get_app_config("texhub.compile_base_dir");
+    let base_compile_dir: String = get_app_config("texhub.texhub_proj_compile_base_dir");
     let proj_base_dir = get_proj_path(&base_compile_dir, created_time);
     let proj_dir = join_paths(&[proj_base_dir, proj_id.to_owned()]);
     return proj_dir;
